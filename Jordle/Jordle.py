79fc75ba36768fe8ec0a87eb739c047183979862
Cy35873 <160684566+Cy35873@users.noreply.github.com>
2025-06-09 11:08:44 +1200

    finalize quiz.py

--- Jordle/Jordle.py
+++ Jordle/Jordle.py
@@ -1,18 +1,20 @@
 import os, sys, random
-kbl=[char for char in 'qwfpbjluyarstgmneioxcdvzkh']
+letters='qwfpbjluyarstgmneioxcdvzkh'
+layout=[char for char in 'qwfpbjluy\narstgmneio\n xcdvzkh']
 def g(str):
     return f'\033[92m{str}\033[0m'
 def y(str):
     return f'\033[33m{str}\033[0m'
 def n(str):
     return f'\033[90m{str}\033[0m'
+keyboard=[n(x) if x in letters else x for x in [char for char in 'qwfpbjluy\narstgmneio\n xcdvzkh']]
 wordsPath=os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), 'WORDS.txt')
 with open(wordsPath, 'r') as f:
     words=f.read().split('\n')
 word=random.choice(words)
 while True:
     print()
-    print(keyboardLayout)
+    print(''.join(keyboard))
     guess=input().lower()
     if len(guess)==5:
         if guess in words:
--- milestone_3/quiz.py
+++ milestone_3/quiz.py
@@ -1,8 +1,10 @@
-correct=0 
-total=0
+correct=0 # stores the amount of CORRECTLY answered questions
+total=0   # stores the amount of answered questions
+
+# define question function
 def jint(q,ca): # this is a function that asks a question, requests a user input, and decides whether if the input is correct.
-    global correct
-    global total
+    global correct # this makes sure that the 'correct' variable is referring to the global variable and not some local one.
+    global total # this makes sure that the 'total' variable is referring to the global variable and not some local one.
     ca=ca.split('`') # splits the answer so multiple answers are allowed. eg a`b will make it so a and b are two accepted answers.
     ua=input('Q: '+q+' \n>> ') # this is the input.
     if ua in ca: # checks if the user input is included in the list of accepted correct answers
@@ -11,11 +13,16 @@ def jint(q,ca): # this is a function that asks a question, requests a user input
     else: # if it is not correct
         print('\033[91mIncorrect\033[0m') # tell the user that their answer is incorrect
     total+=1 # either way, increase the number of answered questions by 1
-print('Welcome to Jint SI quiz. Make sure that your answers are in standard symbolic notation.') # greeting message
+
+# greeting message
+print('Welcome to Jint SI quiz. Make sure that your answers are in standard symbolic notation (eg. kg instead of kilogram, m^2 instead of square meter).')
+
+# questions
 jint('What is the SI unit for mass?','kg')
 jint('What is the SI unit for weight?','N')
 jint('What is the SI unit for density?','kg/m^3')
 jint('What is the SI unit for pressure?','Pa`N/m^2')
 jint('What SI unit is used to measure energy over time?','W')
 jint('What is the SI unit for velocity?','m/s`ms^-1')
-print(f'Your score is {correct}/{total}')
+
+print(f'Your score is {correct}/{total}') # tells you your final score out of the total amount of question
